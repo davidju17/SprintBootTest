1186fce604d7998a8125df055036a5c149c7c0fe
David Monroy <davidju17@gmail.com>
2025-07-20 17:10:13 +1000

    create nav in App.tsx

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,9 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Initial commit :)</h1>    
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
+      <div className="container-fluid">
+        <span className='navbar-brand'>David Monroy</span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>Home</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>Seach Books</a>
+            </li>
+          </ul>
+          <ul className='navbar-nav ms-auto'>
+            <li className='nav-item m-1'>
+              <a className='btn btn-outline-light' type='button' href='#'>Sign in</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
