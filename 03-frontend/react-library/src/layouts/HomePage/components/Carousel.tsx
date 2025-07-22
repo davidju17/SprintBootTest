228636e5affa3e0fc0478e1b76ed629a76aaabdd
David Monroy <davidju17@gmail.com>
2025-07-22 14:37:32 +1000

    Refactor creating HomePage.tsx
    create components folder, move all the components to that folder, transfor app from function to export const App, then wrap App in index.tsx, and fix local address of images due to new folder created

--- 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -1,5 +1,6 @@
 import { ReturnBook } from "./ReturnBook";
 
+
 export const Carousel = () => {
     return (
         <div className="container mt-5" style={{ height: 550 }}>
@@ -46,7 +47,7 @@ export const Carousel = () => {
             <div className="d-lg-none mt-3">
                 <div className="row d-flex justify-content-center align-items-center">
                     <div className="text-center">
-                        <img src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+                        <img src={require("./../../../Images/BooksImages/book-luv2code-1000.png")}
                             width="151"
                             height="233"
                             alt="book"
