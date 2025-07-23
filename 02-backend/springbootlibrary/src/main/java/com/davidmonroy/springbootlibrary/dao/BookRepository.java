103cf03c8cbd806e73ba797135e77d899467e84e
David Monroy <davidju17@gmail.com>
2025-07-23 17:45:34 +1000

    Create BookRepository
    I created BookRepository in the previous commit but forgot to separate it

--- 02-backend/springbootlibrary/src/main/java/com/davidmonroy/springbootlibrary/dao/BookRepository.java
+++ 02-backend/springbootlibrary/src/main/java/com/davidmonroy/springbootlibrary/dao/BookRepository.java
@@ -2,9 +2,7 @@ package com.davidmonroy.springbootlibrary.dao;
 
 import com.davidmonroy.springbootlibrary.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BookRepository extends JpaRepository<Book, Long>
 {
 
