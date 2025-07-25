a6b102d961ec4f67f5baef7feb609f621fd2934f
David Monroy <davidju17@gmail.com>
2025-07-25 14:51:56 +1000

    Moving web to the top and refresh page and add info dinamically

--- 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ 03-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -50,6 +50,8 @@ export const SearchBooksPage = () => {
             setIsLoading(false);
             setHttpError(error.message);
         })
+        // Scroll to top on page change
+        window.scrollTo(0, 0);
     }, [currentPage]);
 
     if (isLoading) {
@@ -126,10 +128,10 @@ export const SearchBooksPage = () => {
                         </div>
                     </div>
                     <div className='mt-3'>
-                        <h5>Number of results: (22)</h5>
+                        <h5>Number of results: ({totalAmountOfBooks})</h5>
                     </div>
                     <p>
-                        1 to 5 of 22 items:
+                        {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
                     </p>
                     {books.map(book => (
                         <SearchBook book={book} key={book.id} />
