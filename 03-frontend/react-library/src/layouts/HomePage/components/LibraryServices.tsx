1144d6395a81be5dc0edaf0d1f053ecc70e249ca
David Monroy <davidju17@gmail.com>
2025-09-10 14:38:05 +1000

    add auth LibraryServices
    Need to update ManageLibraryPage, AddNewBook, AdminMessages, ChangeQuantityOfBook, Messages, PostNewMessage, HistoryPage, Loans when I create them

--- 03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
@@ -1,7 +1,11 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+
 export const LibraryServices = () => {
 
+    const { isAuthenticated } = useAuth0();
 
-    return (
+    return(
         <div className='container my-5'>
             <div className='row p-4 align-items-center border shadow-lg'>
                 <div className='col-lg-7 p-3'>
@@ -13,9 +17,15 @@ export const LibraryServices = () => {
                         send our library admin's a personal message!
                     </p>
                     <div className='d-grid gap-2 justify-content-md-start mb-4 mb-lg-3'>
-                        <a className='btn main-color btn-lg text-white' href='#'>
+                        {isAuthenticated ? 
+                        <Link to='/messages' type='button' className='btn main-color btn-lg px-4 me-md-2 fw-bold text-white'>
+                            Library Services
+                        </Link>   
+                        :
+                        <Link className='btn main-color btn-lg text-white' to='/login'>
                             Sign up
-                        </a> 
+                        </Link> 
+                    }
                     </div>
                 </div>
                 <div className='col-lg-4 offset-lg-1 shadow-lg lost-image'></div>
