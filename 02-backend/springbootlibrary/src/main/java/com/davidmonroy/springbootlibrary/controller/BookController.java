3c898cb64390a3846e0d24cb592fea1ef9f262e1
David Monroy <davidju17@gmail.com>
2025-09-12 20:35:36 +1000

    create BookController with security
    to test PUT http://localhost:8080/api/books/secure/checkout?bookId=1 using Key: Authorization Value: Bearer !@##$$%$%$

---
+++ 02-backend/springbootlibrary/src/main/java/com/davidmonroy/springbootlibrary/controller/BookController.java
@@ -0,0 +1,29 @@
+package com.davidmonroy.springbootlibrary.controller;
+
+import com.davidmonroy.springbootlibrary.entity.Book;
+import com.davidmonroy.springbootlibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController
+{
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService)
+    {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@AuthenticationPrincipal Jwt jwt, @RequestParam Long bookId) throws Exception
+    {
+        String userEmail = "testuser@gmail.com"; //hardcoded for now
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
