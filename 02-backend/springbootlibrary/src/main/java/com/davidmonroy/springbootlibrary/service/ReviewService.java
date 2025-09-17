5a98ecd34ac44951eef4dc006856b0594656c5c8
David Monroy <davidju17@gmail.com>
2025-09-17 19:41:35 +1000

    add review service logic

---
+++ 02-backend/springbootlibrary/src/main/java/com/davidmonroy/springbootlibrary/service/ReviewService.java
@@ -0,0 +1,51 @@
+package com.davidmonroy.springbootlibrary.service;
+
+import com.davidmonroy.springbootlibrary.dao.BookRepository;
+import com.davidmonroy.springbootlibrary.dao.ReviewRepository;
+import com.davidmonroy.springbootlibrary.entity.Review;
+import com.davidmonroy.springbootlibrary.requestmodels.ReviewRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Service
+@Transactional
+public class ReviewService
+{
+    private BookRepository bookRepository;
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(BookRepository bookRepository, ReviewRepository reviewRepository)
+    {
+        this.bookRepository = bookRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception
+    {
+        Review validateReview = reviewRepository.findByUserEmailAndBookId(userEmail, reviewRequest.getBookId());
+        if (validateReview != null)
+        {
+            throw new Exception("User has already created");
+        }
+
+        Review review = new Review();
+        review.setBookId(review.getBookId());
+        review.setRating(review.getRating());
+        review.setUserEmail(userEmail);
+
+        if(reviewRequest.getReviewDescription().isPresent())
+        {
+            review.setReviewDescription(reviewRequest.getReviewDescription().map(
+                    Objects::toString
+            ).orElse(null));
+        }
+        review.setDate(Date.valueOf(LocalDate.now()));
+        reviewRepository.save(review);
+    }
+}
