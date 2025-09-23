b9560c4b66d7cf87129c668c7f6a9fb1901ac7b2
David Monroy <davidju17@gmail.com>
2025-09-23 21:57:07 +1000

    Spring Boot: create MessagesController

---
+++ 02-backend/springbootlibrary/src/main/java/com/davidmonroy/springbootlibrary/controller/MessagesController.java
@@ -0,0 +1,45 @@
+package com.davidmonroy.springbootlibrary.controller;
+
+import com.davidmonroy.springbootlibrary.entity.Message;
+import com.davidmonroy.springbootlibrary.service.MessagesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessagesController {
+
+    private MessagesService messagesService;
+
+    @Autowired
+    public MessagesController(MessagesService messagesService) {
+        this.messagesService = messagesService;
+    }
+
+    @PostMapping("/secure/add/message")
+    public void postMessage(@AuthenticationPrincipal Jwt jwt,
+                            @RequestBody Message messageRequest) {
+        String userEmail = jwt.getClaim("email");
+        messagesService.postMessage(messageRequest, userEmail);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
