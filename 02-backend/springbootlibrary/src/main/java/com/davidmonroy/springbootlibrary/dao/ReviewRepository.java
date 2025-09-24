9728cab107b25cf70c21528ad98dd2065e20861e
David Monroy <davidju17@gmail.com>
2025-09-24 22:47:25 +1000

    add missing imports

--- 02-backend/springbootlibrary/src/main/java/com/davidmonroy/springbootlibrary/dao/ReviewRepository.java
+++ 02-backend/springbootlibrary/src/main/java/com/davidmonroy/springbootlibrary/dao/ReviewRepository.java
@@ -4,6 +4,9 @@ import com.davidmonroy.springbootlibrary.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>
