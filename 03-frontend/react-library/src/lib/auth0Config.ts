25fe39785beef171681712cf3fb6f3cbb731fa70
David Monroy <davidju17@gmail.com>
2025-09-25 22:43:24 +1000

    Update audience to https in FE and BE
    I had to created another API in auth0 since the one I created before is only http://localhost:8080. The new API is https://localhost:8080

--- 03-frontend/react-library/src/lib/auth0Config.ts
+++ 03-frontend/react-library/src/lib/auth0Config.ts
@@ -2,7 +2,7 @@ export const auth0Config =
 {
     clientId: 'VlfPczCDCr2eqnDjvKmIDvmtBnnbwO0k',
     issuer: 'dev-2c5wooghf65q8q51.us.auth0.com',
-    audience: "http://localhost:8080",
+    audience: "https://localhost:8080",
     redirectUri: window.location.origin+"/callback",
     scope: 'openid profile email'
 }
