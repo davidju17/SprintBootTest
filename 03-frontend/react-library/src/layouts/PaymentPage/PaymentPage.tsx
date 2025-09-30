2b5f4a5219f4e0c85c2c29335e96d281897dfc24
David Monroy <davidju17@gmail.com>
2025-09-30 20:06:15 +1000

    FE: create PaymentPage, useEffect and HTML/CSS
    it is not working

---
+++ 03-frontend/react-library/src/layouts/PaymentPage/PaymentPage.tsx
@@ -0,0 +1,83 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import PaymentInfoRequest from '../../models/PaymentInfoRequest';
+import { SpinnerLoading } from '../Utils/SpinnerLoading';
+
+export const PaymentPage = () => {
+    
+    const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const [httpError, setHttpError] = useState(false);
+    const [submitDisabled, setSubmitDisabled] = useState(false);
+    const [fees, setFees] = useState(0);
+    const [loadingFees, setLoadingFees] = useState(true);
+
+    useEffect(() => {
+        const fetchFees = async () => {
+            if (isAuthenticated) {
+                const accessToken = await getAccessTokenSilently();
+                const url = `${process.env.REACT_APP_API}/payments/search/findByUserEmail?userEmail=dummy`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: { 
+                        Authorization: `Bearer ${accessToken}`,
+                        'Content-Type': 'application/json'
+                     }
+                };
+                const paymentResponse = await fetch(url, requestOptions);
+                if (!paymentResponse.ok) {
+                    throw new Error('Something went wrong!')
+                }
+                const paymentResponseJson = await paymentResponse.json();
+                setFees(paymentResponseJson.amount);
+                setLoadingFees(false);
+            }
+        }
+        fetchFees().catch((error: any) => {
+            setLoadingFees(false);
+            setHttpError(error.message);
+        })
+    }, [ isAuthenticated, getAccessTokenSilently]);
+
+    if (loadingFees) {
+        return (
+            <SpinnerLoading/>
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+
+    return(
+        <div className='container'>
+            {fees > 0 && <div className='card mt-3'>
+                <h5 className='card-header'>Fees pending: <span className='text-danger'>${fees}</span></h5>
+                <div className='card-body'>
+                    <h5 className='card-title mb-3'>Credit Card</h5>
+                    <CardElement id='card-element' />
+                    <button disabled={submitDisabled} type='button' className='btn btn-md main-color text-white mt-3'
+                    >
+                        Pay fees
+                    </button>
+                </div>
+            </div>}
+
+            {fees === 0 && 
+                <div className='mt-3'>
+                    <h5>You have no fees!</h5>
+                    <Link type='button' className='btn main-color text-white' to='search'>
+                        Explore top books
+                    </Link>
+                </div>
+            }
+            {submitDisabled && <SpinnerLoading/>}
+        </div>
+    );
+}
