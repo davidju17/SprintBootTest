24fc212ab3aa0dcf6360cec76da729e8ae88400f
David Monroy <davidju17@gmail.com>
2025-09-30 15:50:29 +1000

    FE: index configurations
    allow stripe functions to be used in my project and conf index.tsx

--- 03-frontend/react-library/src/index.tsx
+++ 03-frontend/react-library/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import  { BrowserRouter } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY!);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +15,8 @@ const root = ReactDOM.createRoot(
 // and it will be able to use the routing features provided by react-router-dom.
 root.render(
   <BrowserRouter>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </BrowserRouter>
 );
